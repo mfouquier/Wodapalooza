66405afa4919ebc0e2142cc5c71c02a31a152f80
Matthew Fouquier <mfouquier1@gmail.com>
2022-04-20 15:11:14 -0400

    added a splash screen activity

---
+++ app/src/main/java/com/fouq/wodapalooza/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.fouq.wodapalooza;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Handler mHandler;
+    Runnable mRunnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        mHandler = new Handler();
+        mRunnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        // its trigger runnable after 4000 millisecond.
+        mHandler.postDelayed(mRunnable, 2000);
+
+    }
+}
+
